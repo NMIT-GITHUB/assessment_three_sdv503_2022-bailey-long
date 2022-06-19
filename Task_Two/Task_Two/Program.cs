12f7b066d8b297bf240b84ba7fd4406a4cedfbc6
Loopypew <fivebailey2@gmail.com>
2022-06-19 17:42:06 +1200

    Task Two Complete

--- Task_Two/Task_Two/Program.cs
+++ Task_Two/Task_Two/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿int[] newArray = { 0,0,0 };
+void arrSort(int[][] arr)
+{
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            int largest = arr[i][0];
+            for (int j = 0; j < arr.GetLength(0); j++)
+            {
+                if (arr[i][j] > largest)
+                {
+                    largest = arr[i][j];
+                    newArray[i] = largest;
+                }
+            }
+        }
+    Console.WriteLine(String.Join(",", newArray.Cast<int>()));
+}
+int[][] array = new int[2][];
+array[0] = new int[] { 4, 2, 7, 1 };
+array[1] = new int[] { 20, 70, 40, 90 };
+array[2] = new int[] { 1, 2, 0 };
+arrSort(array);
